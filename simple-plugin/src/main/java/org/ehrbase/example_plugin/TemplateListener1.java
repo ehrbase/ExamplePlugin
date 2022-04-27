e29e5b776995efc62a06fc67fe5f38c97aba905e
vinzenz.mueller <vinzenz.mueller@vitagroup.ag>
2022-04-27 17:40:08 +0200

    adjust to fix in AbstractTemplateExtensionPoint

--- simple-plugin/src/main/java/org/ehrbase/example_plugin/TemplateListener1.java
+++ simple-plugin/src/main/java/org/ehrbase/example_plugin/TemplateListener1.java
@@ -1,8 +1,6 @@
 package org.ehrbase.example_plugin;
 
-import java.util.UUID;
 import java.util.function.Function;
-import org.ehrbase.plugin.dto.EhrStatusWithEhrId;
 import org.ehrbase.plugin.extensionpoints.AbstractTemplateExtensionPoint;
 import org.openehr.schemas.v1.OPERATIONALTEMPLATE;
 import org.pf4j.Extension;
@@ -14,13 +12,13 @@ public class TemplateListener1 extends AbstractTemplateExtensionPoint {
   private static final Logger log = LoggerFactory.getLogger(TemplateListener1.class);
 
   @Override
-  public EhrStatusWithEhrId beforeCreation(EhrStatusWithEhrId input) {
+  public OPERATIONALTEMPLATE beforeCreation(OPERATIONALTEMPLATE input) {
     log.info("Before Template Creation TemplateListener1");
     return super.beforeCreation(input);
   }
 
   @Override
-  public UUID afterCreation(UUID output) {
+  public String afterCreation(String output) {
     log.info("After Template Creation TemplateListener1");
     return super.afterCreation(output);
   }
